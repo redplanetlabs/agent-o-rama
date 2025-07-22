28fc2b71f1f7602d9fb306eff42d5345607433eb
Tommy Morriss <thmorriss@gmail.com>
2025-07-22 15:31:38 -0400

    componentify repl setup

--- ui/dev/repl.clj
+++ ui/dev/repl.clj
@@ -2,24 +2,62 @@
   (:require
    [com.rpl.agent-o-rama.ui.server :as srv]
    [shadow.cljs.devtools.api :as shadow]
+   [com.stuartsierra.component :as component]
 
    [ring.adapter.jetty :as jetty]))
-    
-(defonce jetty-ref (atom nil))
 
-(defn start []
-  (shadow/watch :frontend)
-  (reset! jetty-ref
-    (jetty/run-jetty #'srv/handler
-      {:port 3000
-       :join? false}))
-  ::started)
+(defrecord ShadowComponent []
+  component/Lifecycle
+  (start [component]
+    (shadow/watch :frontend)
+    (assoc component :shadow-started? true))
+  
+  (stop [component]
+    (when (:shadow-started? component) (shadow/stop-worker :frontend))
+    (dissoc component :shadow-started?)))
+
+(defn new-shadow-component []
+  (->ShadowComponent))
+
+(defrecord WebServerComponent [port handler jetty-server]
+  component/Lifecycle
+  (start [component]
+    (if jetty-server
+      (do
+        (println "Webserver already running on port" port)
+        component)
+      (do
+        (println "Starting webserver on port" port)
+        (let [server (jetty/run-jetty handler
+                                      {:port port
+                                       :join? false})]
+          (assoc component :jetty-server server)))))
+  
+  (stop [component]
+    (when jetty-server
+      (println "Stopping webserver")
+      (.stop jetty-server))
+    (assoc component :jetty-server nil)))
+
+(defn new-webserver-component [port handler]
+  (->WebServerComponent port handler nil))
+
+(defn new-system []
+  (component/system-map
+   :shadow (new-shadow-component)
+   :webserver (new-webserver-component 3000 #'srv/handler)))
+
+(defonce system (atom nil))
 
 (defn stop []
-  (when-some [jetty @jetty-ref]
-    (reset! jetty-ref nil)
-    (.stop jetty))
+  (when @system (reset! system (component/stop @system)))
   ::stopped)
+
+(defn start []
+  (when @system
+    (stop))
+  (reset! system (component/start (new-system)))
+  ::started)
 
 (defn go []
   (stop)
