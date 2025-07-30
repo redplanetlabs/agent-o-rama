69fe3f2807ab3b13ffcf3a6741063f2be6caa825
Nathan Marz <nathan@redplanetlabs.com>
2025-07-30 09:46:42 -1000

    progress

--- test/clj/com/rpl/langchain4j_test.clj
+++ test/clj/com/rpl/langchain4j_test.clj
@@ -23,7 +23,8 @@
    [dev.langchain4j.data.document
     Document]
    [dev.langchain4j.data.message
-    SystemMessage]
+    SystemMessage
+    UserMessage]
    [dev.langchain4j.model.openai
     OpenAiChatModel
     OpenAiStreamingChatModel]
@@ -97,8 +98,8 @@ When you are satisfied with your understanding, complete the interview with: \"T
 Remember to stay in character throughout your response, reflecting the persona and goals provided to you.")
 
 (defn generate-question-instructions
-  [analyst]
-  (format GENERATE-QUESTION-INSTRUCTIONS (analyst-persona analyst)))
+  [persona]
+  (format GENERATE-QUESTION-INSTRUCTIONS persona))
 
 (def SEARCH-INSTRUCTIONS
   "You will be given a conversation between an analyst and an expert.
@@ -120,6 +121,91 @@ Convert this final question into a well-structured web search query no more than
   "<Document source=\"%s\" page=\"%s\">
 %s
 </Document>")
+
+(def ANSWER-INSTRUCTIONS
+  "You are an expert being interviewed by an analyst.
+
+Here is analyst area of focus: %s.
+
+You goal is to answer a question posed by the interviewer.
+
+To answer question, use this context:
+
+%s
+
+When answering questions, follow these guidelines:
+
+1. Use only the information provided in the context.
+
+2. Do not introduce external information or make assumptions beyond what is explicitly stated in the context.
+
+3. The context contain sources at the topic of each individual document.
+
+4. Include these sources your answer next to any relevant statements. For example, for source # 1 use [1].
+
+5. List your sources in order at the bottom of your answer. [1] Source 1, [2] Source 2, etc
+
+6. If the source is: <Document source=\"assistant/docs/llama3_1.pdf\" page=\"7\"/>' then just list:
+
+[1] assistant/docs/llama3_1.pdf, page 7
+
+And skip the addition of the brackets as well as the Document source preamble in your citation.")
+
+(defn answer-instructions
+  [persona context]
+  (format ANSWER-INSTRUCTIONS persona context))
+
+(def SECTION-WRITER-INSTRUCTIONS
+  "You are an expert technical writer.
+
+Your task is to create a short, easily digestible section of a report based on a set of source documents.
+
+1. Analyze the content of the source documents:
+- The name of each source document is at the start of the document, with the <Document tag.
+
+2. Create a report structure using markdown formatting:
+- Use ## for the section title
+- Use ### for sub-section headers
+
+3. Write the report following this structure:
+a. Title (## header)
+b. Summary (### header)
+c. Sources (### header)
+
+4. Make your title engaging based upon the focus area of the analyst:
+%s
+
+5. For the summary section:
+- Set up summary with general background / context related to the focus area of the analyst
+- Emphasize what is novel, interesting, or surprising about insights gathered from the interview
+- Create a numbered list of source documents, as you use them
+- Do not mention the names of interviewers or experts
+- Aim for approximately 400 words maximum
+- Use numbered sources in your report (e.g., [1], [2]) based on information from source documents
+
+6. In the Sources section:
+- Include all sources used in your report
+- Provide full links to relevant websites or specific document paths
+- Separate each source by a newline. Use two spaces at the end of each line to create a newline in Markdown.
+- It will look like:
+
+### Sources
+[1] Link or Document name
+[2] Link or Document name
+
+7. Be sure to combine sources. For example this is not correct:
+
+[3] https://ai.meta.com/blog/meta-llama-3-1/
+[4] https://ai.meta.com/blog/meta-llama-3-1/
+
+There should be no redundant sources. It should simply be:
+
+[3] https://ai.meta.com/blog/meta-llama-3-1/
+
+8. Final review:
+- Ensure the report follows the required structure
+- Include no preamble before the title of the report
+- Check that all guidelines have been followed")
 
 (def MAPPER (j/object-mapper {:decode-key-fn keyword}))
 
@@ -171,6 +257,34 @@ Convert this final question into a well-structured web search query no more than
    (.search tavily
             (WebSearchRequest/from terms (int max-results)))))
 
+(defn generate-search-query*
+  [openai messages]
+  (-> (lc4j/chat openai
+                 (concat [(SystemMessage. SEARCH-INSTRUCTIONS)]
+                         messages))
+      .aiMessage
+      .text))
+
+(defn generate-search-query
+  [openai messages]
+  (loop [messages messages
+         iters    0]
+    (when (>= iters 3)
+      (throw (ex-info "Failed to generate search query <= 400 chars"
+                      {:messages (str messages)})))
+    (let [q (generate-search-query* openai messages)]
+      (if (< (count q) 400)
+        q
+        (recur
+         (conj
+          messages
+          (UserMessage.
+           (format
+            "You last generated: %s\nTry again and keep the query under 400 chars."
+            q)))
+         (inc iters))
+      ))))
+
 (aor/defagentmodule ResearchAgentModule
   [topology]
   (aor/declare-agent-object topology
@@ -198,8 +312,10 @@ Convert this final question into a well-structured web search query no more than
     (aor/node
      "create-analysts"
      "questions"
-     (fn [agent-node topic human-feedback max-analysts]
-       (let [openai (aor/get-agent-object agent-node "openai")
+     (fn [agent-node topic human-feedback options]
+       (let [{:keys [max-analysts max-turns]}
+             (merge {:max-analysts 4 :max-turns 2} options)
+             openai (aor/get-agent-object agent-node "openai")
              res    (-> openai
                         (lc4j/chat
                          (lc4j/chat-request
@@ -212,34 +328,33 @@ Convert this final question into a well-structured web search query no more than
                         .aiMessage
                         .text
                         (j/read-value MAPPER))]
-         (aor/emit! agent-node "questions" (:analysts res))
+         (aor/emit! agent-node "questions" (:analysts res) max-turns)
        )))
     (aor/agg-start-node
      "questions"
      "generate-question"
-     (fn [agent-node analysts]
+     (fn [agent-node analysts max-turns]
        (doseq [analyst analysts]
-         (aor/emit! agent-node "generate-question" analyst []))))
+         (aor/emit! agent-node
+                    "generate-question"
+                    (analyst-persona analyst)
+                    []
+                    max-turns))))
     (aor/agg-start-node
      "generate-question"
      ["search-web" "search-wikipedia"]
-     (fn [agent-node analyst messages]
+     (fn [agent-node persona messages max-turns]
        (let [openai       (aor/get-agent-object agent-node "openai")
-             instr        (generate-question-instructions analyst)
+             instr        (generate-question-instructions persona)
              question     (-> (lc4j/chat openai
                                          (concat [(SystemMessage. instr)]
                                                  messages))
                               .aiMessage)
              new-messages (conj messages question)
-             search-query (-> (lc4j/chat openai
-                                         (concat [(SystemMessage.
-                                                   SEARCH-INSTRUCTIONS)]
-                                                 new-messages))
-                              .aiMessage
-                              .text)]
+             search-query (generate-search-query openai new-messages)]
          (aor/emit! agent-node "search-web" search-query)
          (aor/emit! agent-node "search-wikipedia" search-query)
-         new-messages
+         {:persona persona :messages new-messages :max-turns max-turns}
        )))
     (aor/node
      "search-web"
@@ -271,31 +386,57 @@ Convert this final question into a well-structured web search query no more than
        )))
     (aor/agg-node
      "agg-research"
-     ["generate-question" "agg-analysts"]
+     ["generate-question" "write-section"]
      aggs/+vec-agg
-     (fn [agent-node searches messages]
-       (atomic-println "SEARCH RESULTS")
-       (doseq [s searches]
-         (atomic-println s))
-       ;; TODO: <<<<>>>>
-     ))
-    (aor/agg-node
-     "agg-analysts"
-     "write-section"
-     aggs/+vec-agg
-     (fn [agent-node agg-state node-start-res]
-       ;; TODO: <<<<>>>>
-       (aor/emit! agent-node "write-section" nil)
-     ))
+     (fn [agent-node searches {:keys [persona messages max-turns]}]
+       (let [openai       (aor/get-agent-object agent-node "openai")
+             instr        (answer-instructions persona
+                                               (str/join "\n---\n" searches))
+             answer       (-> (lc4j/chat openai
+                                         (concat [(SystemMessage. instr)]
+                                                 messages))
+                              .aiMessage
+                              .text)
+             new-messages (conj messages (UserMessage. "expert" answer))
+             num-turns    (count
+                           (filter
+                            (fn [m]
+                              (and (instance? UserMessage m)
+                                   (= "expert" (.name ^UserMessage m))))
+                            new-messages))]
+         (atomic-println "PERSONA:" persona)
+         (atomic-println "MESSAGES:" new-messages)
+         (if (>= num-turns max-turns)
+           (aor/emit! agent-node
+                      "write-section"
+                      persona
+                      new-messages
+                      searches)
+           (aor/emit! agent-node
+                      "generate-question"
+                      persona
+                      new-messages
+                      max-turns))
+       )))
     (aor/node
      "write-section"
-     nil
-     (fn [agent-node data]
-       ;; TODO: <<<<>>>>
-       (aor/result! agent-node "done")
+     "agg-sections"
+     (fn [agent-node persona messages searches]
+
      ))
-
-
+    (aor/agg-node
+     "agg-sections"
+     nil
+     aggs/+vec-agg
+     (fn [agent-node sections _]
+       ;; TODO: <<<<>>>>
+       ;; - input is:
+       ;;   - interview, which is messages converted to a string
+       ;;   - context, which is all the web searches for the analyst (across all
+       ;;   rounds?)
+       ;;   - analyst persona
+       (aor/result! agent-node nil)
+     ))
   ))
 
 (deftest research-agent-test
@@ -322,7 +463,7 @@ Convert this final question into a well-structured web search query no more than
           foo
           "The movie Touch of Evil"
           "Want to understand the impact of this movie on the film industry"
-          4))
+          {}))
 
        (bind agent-task-id (.getTaskId inv))
        (bind agent-id (.getAgentInvokeId inv))
