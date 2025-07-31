884581c80019e97ccae78414932577cb843275fe
Nathan Marz <nathan@redplanetlabs.com>
2025-07-30 14:37:52 -1000

    progress

--- test/clj/com/rpl/langchain4j_test.clj
+++ test/clj/com/rpl/langchain4j_test.clj
@@ -27,6 +27,36 @@
   (when (some? (System/getenv "OPENAI_API_KEY"))
     (with-open [ipc (rtest/create-ipc)]
       (letlocals
-       ;; TODO: <<<<>>>>
+       (bind module
+         (aor/agentmodule
+          [topology]
+          (aor/declare-agent-object topology
+                                    "openai-api-key"
+                                    (System/getenv "OPENAI_API_KEY"))
+          (aor/declare-agent-object-builder
+           topology
+           "openai"
+           (fn [setup]
+             (-> (OpenAiStreamingChatModel/builder)
+                 (.apiKey (aor/get-agent-object setup "openai-api-key"))
+                 (.modelName "gpt-4o-mini")
+                 .build)))
+          (->
+            topology
+            (aor/new-agent "foo")
+            (aor/node
+             "start"
+             nil
+             (fn [agent-node prompt]
+               (let [openai (aor/get-agent-object agent-node "openai")]
+                 (aor/result! agent-node (lc4j/basic-chat openai prompt))
+               )))
+          )))
+       (rtest/launch-module! ipc module {:tasks 4 :threads 2})
+       (bind module-name (get-module-name module))
+       (bind agent-manager (aor/agent-manager ipc module-name))
+       (bind foo (aor/agent-client agent-manager "foo"))
 
+       (bind res (aor/agent-invoke foo "What is 11*13?"))
+       (is (str/includes? res "143"))
       ))))
