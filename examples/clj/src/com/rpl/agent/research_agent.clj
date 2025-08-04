8f6cc6202d77ccf842c1b1735e5168381aed6e33
Nathan Marz <nathan@redplanetlabs.com>
2025-08-03 16:23:48 -1000

    clean up

--- examples/clj/src/com/rpl/agent/research_agent.clj
+++ examples/clj/src/com/rpl/agent/research_agent.clj
@@ -612,8 +612,7 @@ Here are the sections to reflect on for writing: %s")
 
 (defn run-research-agent
   []
-  (with-open [ipc (rtest/create-ipc)
-              ui  (aor/start-ui ipc)]
+  (with-open [ipc (rtest/create-ipc)]
     (rtest/launch-module! ipc ResearchAgentModule {:tasks 4 :threads 2})
     (let [module-name   (get-module-name ResearchAgentModule)
           agent-manager (aor/agent-manager ipc module-name)
