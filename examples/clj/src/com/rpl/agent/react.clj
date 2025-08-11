354d9384bf9ff91d1838f2f1f5c0577f3e50044a
Hugo Duncan <hugo@hugoduncan.org>
2025-08-11 15:06:07 -0400

    attempt to use tools agent

--- examples/clj/src/com/rpl/agent/react.clj
+++ examples/clj/src/com/rpl/agent/react.clj
@@ -4,19 +4,17 @@
   (:require
    [clojure.string :as str]
    [com.rpl.agent-o-rama :as aor]
-   [com.rpl.agent-o-rama.impl.helpers :as h]
    [com.rpl.agent-o-rama.langchain4j :as lc4j]
    [com.rpl.agent-o-rama.langchain4j.json :as lj]
+   [com.rpl.agent-o-rama.tools :as tools]
    [com.rpl.rama :as rama]
-   [com.rpl.rama.aggs :as aggs]
    [com.rpl.rama.test :as rtest]
    [jsonista.core :as j])
   (:import
    [com.rpl.agentorama AgentComplete]
-   [dev.langchain4j.agent.tool ToolExecutionRequest]
    [dev.langchain4j.data.document Document]
-   [dev.langchain4j.data.message SystemMessage
-    ToolExecutionResultMessage
+   [dev.langchain4j.data.message
+    SystemMessage
     UserMessage]
    [dev.langchain4j.model.openai OpenAiChatModel]
    [dev.langchain4j.web.search
@@ -35,8 +33,8 @@
 
 (defn mk-tavily-search [{:keys [max-results] :or {max-results 3}}]
   (fn tavily-search
-    [agent-node arguments]
-    (let [terms                         (:terms arguments)
+    [agent-node _ arguments]
+    (let [terms                         (get arguments "terms")
           ^TavilyWebSearchEngine tavily (aor/get-agent-object
                                          agent-node
                                          "tavily")
@@ -51,41 +49,17 @@
         (.toDocuments
          (.search tavily search-results)))))))
 
-(defn- tavily-tool-spec []
-  (lc4j/tool-specification
-   "tavily"
-   (lj/object
-    {:description "Map containing the terms to search for"
-     :required    ["terms"]}
-    {"terms" (lj/string "The terms to search for")})
-   "Search the web"))
-
-(defn- tool-specs []
-  [(tavily-tool-spec)])
-
-(def ^:private TOOL-CALL-ERROR-TEMPLATE
-  "Error: %s\n Please fix your mistakes.")
-
-(defn tool-error-message
-  [e]
-  (format TOOL-CALL-ERROR-TEMPLATE e))
-
-(defn- wrap-tool-fn
-  [tool-fn]
-  (fn wrapped-tool-fn [id tool-name agent-node arguments]
-    (try
-      (ToolExecutionResultMessage.
-       id
-       tool-name
-       (tool-fn agent-node arguments))
-      (catch Exception e
-        ;; NOTE we don't seem able to set the `status` field to `error`
-        (ToolExecutionResultMessage.
-         id
-         tool-name
-         (tool-error-message e))))))
-
-(def tool-fns {"tavily" (wrap-tool-fn (mk-tavily-search {:max-results 3}))})
+(def TOOLS
+  [(tools/tool-info
+    (tools/tool-specification
+     "tavily"
+     (lj/object
+      {:description "Map containing the terms to search for"
+       :required    ["terms"]}
+      {"terms" (lj/string "The terms to search for")})
+     "Search the web")
+    (mk-tavily-search {:max-results 3})
+    {:include-context? true})])
 
 (def ^:private SYSTEM-PROMPT "You are a helpful AI assistant.
 
@@ -130,83 +104,29 @@ System time: %s")
 
    (aor/node
     "chat"
-    ["tools" "chat"]
+    ["chat"]
     (fn chat-fn [agent-node messages]
       (let [openai       (aor/get-agent-object agent-node "openai")
-            chat-options {:tool-specifications (tool-specs)}
+            tools-exec   (aor/agent-client agent-node "tools-execution")
+            chat-options {:tools TOOLS}
             response     (lc4j/chat
                           openai
                           (lc4j/chat-request messages chat-options))
             ai-message   (.aiMessage response)
-            tool-calls   (vec (.toolExecutionRequests ai-message))
-            messages     (conj messages ai-message)]
-        (if (seq tool-calls)
-          (aor/emit! agent-node
-                     "tools"
-                     tool-calls
-                     messages)
-          (aor/result! agent-node {:messages messages})))))
-
-   (aor/agg-start-node
-    ;; Handle all LLM tool calls from an AiResponse
-    "tools"
-    "tool"
-    (fn tools-fn [agent-node tool-calls messages]
-      (doseq [^ToolExecutionRequest tool-exec-req tool-calls]
-        (let [id          (.id tool-exec-req)
-              tool-name   (.name tool-exec-req)
-              args-string (.arguments tool-exec-req)]
-          (aor/emit!
-           agent-node
-           "tool"
-           id
-           tool-name
-           args-string)))
-      {:messages messages}))
-
-   (aor/node
-    ;; Handle one LLM tool execution request
-    "tool"
-    "tool-results-agg"
-    (fn tool-node [agent-node id tool-name arguments]
-      (let [start-time-millis                      (h/current-time-millis)
-            data                                   (j/read-value
-                                                    arguments
-                                                    MAPPER)
-            tool-fn                                (tool-fns tool-name)
-            ^ToolExecutionResultMessage result-msg (tool-fn
-                                                    id
-                                                    tool-name
-                                                    agent-node
-                                                    data)
-            end-time-millis                        (h/current-time-millis)]
-        (aor/record-nested-op!
-         agent-node
-         :other
-         start-time-millis
-         end-time-millis
-         {"tool-call-id" id
-          "tool-name"    tool-name
-          "arguments"    data
-          "result"       (.text result-msg)})
-        (aor/emit!
-         agent-node
-         "tool-results-agg"
-         result-msg))))
-
-   (aor/agg-node
-    "tool-results-agg"
-    ["chat"]
-    aggs/+vec-agg
-    (fn [agent-node results {:keys [messages]}]
-      (aor/emit!
-       agent-node
-       "chat"
-       (into messages results))))))
+            tool-calls   (not-empty (vec (.toolExecutionRequests ai-message)))]
+        (if tool-calls
+          (let [tool-results  (aor/agent-invoke tools-exec tool-calls)
+                _             (prn :tool-results tool-results)
+                next-messages (into (conj messages ai-message) tool-results)]
+            (prn :results tool-results)
+            (aor/emit! agent-node "chat" next-messages))
+          (aor/result! agent-node {:messages (conj messages ai-message)}))))))
+  (tools/new-tools-agent topology "tools-execution" TOOLS))
 
 (defn run-agent
   []
-  (with-open [ipc (rtest/create-ipc)]
+  (with-open [ipc (rtest/create-ipc)
+              _   (aor/start-ui ipc)]
     (rtest/launch-module! ipc ReActModule {:tasks 4 :threads 2})
     (let [module-name   (rama/get-module-name ReActModule)
           agent-manager (aor/agent-manager ipc module-name)
