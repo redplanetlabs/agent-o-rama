f9e72823fd9f6fdd94ffd43bf5a76846d08538d2
Hugo Duncan <hugo@hugoduncan.org>
2025-08-21 19:25:42 -0400

    fix: add ping/pong handlers to sente.clj

--- src/clj/com/rpl/agent_o_rama/impl/ui/sente.clj
+++ src/clj/com/rpl/agent_o_rama/impl/ui/sente.clj
@@ -55,6 +55,8 @@
                   :error (.getMessage e)}))))
 
 ;; do nothing
+(defmethod -event-msg-handler :chsk/ws-ping [ev-msg])
+(defmethod -event-msg-handler :chsk/ws-pong [ev-msg])
 (defmethod -event-msg-handler :chsk/uidport-open [{:as ev-msg :keys [uid]}])
 (defmethod -event-msg-handler :chsk/uidport-close [{:as ev-msg :keys [uid]}])
 
