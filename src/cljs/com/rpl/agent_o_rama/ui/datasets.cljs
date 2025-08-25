2638ed2cd8dd5ce00f87d6f8e8c93948c0c135e7
Tommy Morriss <thmorriss@gmail.com>
2025-08-25 15:39:56 -0700

    form

--- src/cljs/com/rpl/agent_o_rama/ui/datasets.cljs
+++ src/cljs/com/rpl/agent_o_rama/ui/datasets.cljs
@@ -78,7 +78,24 @@
                ($ :textarea {:className "w-full h-32 p-3 border rounded-md font-mono text-sm resize-y transition-colors border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                              :value output-schema
                              :placeholder example-schema
-                             :onChange #(set-output-schema (.. % -target -value))})))
+                             :onChange #(set-output-schema (.. % -target -value))}))
+
+            ;; JSON Schema Help Box
+            ($ :div.bg-blue-50.border.border-blue-200.rounded-md.p-4
+               ($ :div.flex
+                  ($ :div.flex-shrink-0
+                     ($ :svg {:className "h-5 w-5 text-blue-400" :fill "currentColor" :viewBox "0 0 20 20"}
+                        ($ :path {:fillRule "evenodd" :d "M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" :clipRule "evenodd"})))
+                  ($ :div.ml-3
+                     ($ :h3.text-sm.font-medium.text-blue-800 "JSON Schema Guidelines")
+                     ($ :div.mt-2.text-sm.text-blue-700
+                        ($ :ul.list-disc.space-y-1.pl-5
+                           ($ :li
+                              "Follow "
+                              ($ :a.underline.hover:text-blue-900 {:href "https://json-schema.org/" :target "_blank"} "JSON Schema")
+                              " specification")
+                           ($ :li "AOR supports " ($ :code.bg-blue-100.px-1.rounded "x-javaType") " extension to reference Java types")
+                           ($ :li "Do not include " ($ :code.bg-blue-100.px-1.rounded "$schema") " or " ($ :code.bg-blue-100.px-1.rounded "$vocabulary") " keys - these are added automatically")))))))
 
          (when error-msg
            ($ :div.mt-4.p-3.bg-red-50.border.border-red-200.rounded-md
