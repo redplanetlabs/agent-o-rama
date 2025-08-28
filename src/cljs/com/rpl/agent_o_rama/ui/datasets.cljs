866ceece27fbba3abdc0cf0767f25608d66d6855
Tommy Morriss <thmorriss@gmail.com>
2025-08-28 14:06:20 -0700

    moved things around

--- src/cljs/com/rpl/agent_o_rama/ui/datasets.cljs
+++ src/cljs/com/rpl/agent_o_rama/ui/datasets.cljs
@@ -47,62 +47,69 @@
                                  :output-schema (:value output-schema-field)
                                  :on-success on-success}))]
 
-    ($ :div
-       ($ :div.space-y-4
-          ($ forms/form-field {:label "Name"
-                               :value (:value name-field)
-                               :on-change (:set-value name-field)
-                               :error (:error name-field)
-                               :required? true})
-          ($ forms/form-field {:label "Description"
-                               :type :textarea
-                               :value (:value description-field)
-                               :on-change (:set-value description-field)
-                               :placeholder "Optional description for this dataset"
-                               :rows 3})
-          ($ :div.grid.grid-cols-2.gap-4
-             ($ forms/form-field {:label "Input JSON Schema"
-                                  :type :textarea
-                                  :value (:value input-schema-field)
-                                  :on-change (:set-value input-schema-field)
-                                  :error (:error input-schema-field)
-                                  :placeholder example-schema
-                                  :rows 20
-                                  :class-name "font-mono"})
-             ($ forms/form-field {:label "Output JSON Schema"
-                                  :type :textarea
-                                  :value (:value output-schema-field)
-                                  :on-change (:set-value output-schema-field)
-                                  :error (:error output-schema-field)
-                                  :placeholder example-schema
-                                  :rows 20
-                                  :class-name "font-mono"}))
+    ;; Modal structure: Header (handled by modal) + Scrollable Body + Fixed Footer
+    ($ :div.flex.flex-col.h-96
+       ;; Scrollable body section
+       ($ :div.flex-1.min-h-0.overflow-y-auto
+          ($ :div.p-6
+             ($ :div.space-y-4
+                ($ forms/form-field {:label "Name"
+                                     :value (:value name-field)
+                                     :on-change (:set-value name-field)
+                                     :error (:error name-field)
+                                     :required? true})
+                ($ forms/form-field {:label "Description"
+                                     :type :textarea
+                                     :value (:value description-field)
+                                     :on-change (:set-value description-field)
+                                     :placeholder "Optional description for this dataset"
+                                     :rows 3})
+                ($ :div.grid.grid-cols-2.gap-4
+                   ($ forms/form-field {:label "Input JSON Schema"
+                                        :type :textarea
+                                        :value (:value input-schema-field)
+                                        :on-change (:set-value input-schema-field)
+                                        :error (:error input-schema-field)
+                                        :placeholder example-schema
+                                        :rows 15
+                                        :class-name "font-mono"})
+                   ($ forms/form-field {:label "Output JSON Schema"
+                                        :type :textarea
+                                        :value (:value output-schema-field)
+                                        :on-change (:set-value output-schema-field)
+                                        :error (:error output-schema-field)
+                                        :placeholder example-schema
+                                        :rows 15
+                                        :class-name "font-mono"}))
 
-            ;; JSON Schema Help Box
-          ($ :div.bg-blue-50.border.border-blue-200.rounded-md.p-4
-             ($ :div.flex
-                ($ :div.flex-shrink-0
-                   ($ :svg {:className "h-5 w-5 text-blue-400" :fill "currentColor" :viewBox "0 0 20 20"}
-                      ($ :path {:fillRule "evenodd" :d "M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" :clipRule "evenodd"})))
-                ($ :div.ml-3
-                   ($ :h3.text-sm.font-medium.text-blue-800 "JSON Schema Guidelines")
-                   ($ :div.mt-2.text-sm.text-blue-700
-                      ($ :ul.list-disc.space-y-1.pl-5
-                         ($ :li
-                            "Follow "
-                            ($ :a.underline.hover:text-blue-900 {:href "https://json-schema.org/" :target "_blank"} "JSON Schema")
-                            " specification")
-                         ($ :li "AOR supports " ($ :code.bg-blue-100.px-1.rounded "x-javaType") " extension to reference Java types")
-                         ($ :li "Do not include " ($ :code.bg-blue-100.px-1.rounded "$schema") " or " ($ :code.bg-blue-100.px-1.rounded "$vocabulary") " keys - these are added automatically")))))))
+                ;; JSON Schema Help Box
+                ($ :div.bg-blue-50.border.border-blue-200.rounded-md.p-4
+                   ($ :div.flex
+                      ($ :div.flex-shrink-0
+                         ($ :svg {:className "h-5 w-5 text-blue-400" :fill "currentColor" :viewBox "0 0 20 20"}
+                            ($ :path {:fillRule "evenodd" :d "M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" :clipRule "evenodd"})))
+                      ($ :div.ml-3
+                         ($ :h3.text-sm.font-medium.text-blue-800 "JSON Schema Guidelines")
+                         ($ :div.mt-2.text-sm.text-blue-700
+                            ($ :ul.list-disc.space-y-1.pl-5
+                               ($ :li
+                                  "Follow "
+                                  ($ :a.underline.hover:text-blue-900 {:href "https://json-schema.org/" :target "_blank"} "JSON Schema")
+                                  " specification")
+                               ($ :li "AOR supports " ($ :code.bg-blue-100.px-1.rounded "x-javaType") " extension to reference Java types")
+                               ($ :li "Do not include " ($ :code.bg-blue-100.px-1.rounded "$schema") " or " ($ :code.bg-blue-100.px-1.rounded "$vocabulary") " keys - these are added automatically"))))))
 
-       (when error
-         ($ forms/form-error {:error error}))
+                ;; Error display inside scrollable area
+                (when error
+                  ($ forms/form-error {:error error}))))
 
-       ($ forms/form-actions {:on-cancel #(state/dispatch [:modal/hide])
-                              :on-submit handle-create
-                              :submit-text "Create Dataset"
-                              :submitting? submitting?
-                              :disabled? (not is-valid?)}))))
+       ;; Fixed footer section
+       ($ :div.flex-shrink-0.border-t.border-gray-200.bg-white.px-6.py-4
+          ($ forms/form-actions {:on-cancel #(state/dispatch [:modal/hide])
+                                 :on-submit handle-create
+                                 :submit-text "Create Dataset"
+                                 :submitting? submitting?
+                                 :disabled? (not is-valid?)}))))))
 
 (defui EditDatasetForm [{:keys [module-id dataset-id initial-name initial-description on-success]}]
   (let [name-field (forms/use-form-state initial-name [forms/required])
