de2d6ab5475c9d96502593090acde1aebd469b33
Tommy Morriss <thmorriss@gmail.com>
2025-09-08 16:19:23 -0700

    tooltip!!! ree

--- src/cljs/com/rpl/agent_o_rama/ui/evaluators.cljs
+++ src/cljs/com/rpl/agent_o_rama/ui/evaluators.cljs
@@ -64,50 +64,28 @@
 
 (defui JsonPathTooltip []
   (let [[open? set-open!] (uix/use-state false)
-        [timeout-id setTimeout-id] (uix/use-state nil)
-        [is-hovering? setIs-hovering!] (uix/use-state false)
+        [timeout-id set-timeout-id!] (uix/use-state nil)
+        [is-hovering? set-is-hovering!] (uix/use-state false)
 
-        ;; Clear existing timeout and set new one
-        schedule-close (fn []
+        schedule-close (fn schedule-close []
+                         (println "schedule close")
                          (when timeout-id
                            (js/clearTimeout timeout-id))
-                         (let [new-timeout (js/setTimeout #(when-not is-hovering? (set-open! false)) 2000)]
-                           (setTimeout-id new-timeout)))
-
-        ;; Cancel timeout and keep open
-        keep-open (fn []
-                    (when timeout-id
-                      (js/clearTimeout timeout-id)
-                      (setTimeout-id nil)))]
+                         (let [new-timeout (js/setTimeout #(if is-hovering?
+                                                             (schedule-close)
+                                                             (set-open! false)) 100)]
+                           (set-timeout-id! new-timeout)))]
 
     ($ :div.relative.inline-flex.items-center
-       {:onMouseEnter (fn []
-                        (set-open! true)
-                        (setIs-hovering! true)
-                        (keep-open))
-        :onMouseLeave (fn []
-                        (setIs-hovering! false)
-                        (schedule-close))}
        ($ InformationCircleIcon {:className "h-4 w-4 text-gray-400 hover:text-blue-500 cursor-help"
-                                 :onClick (fn []
-                                            (set-open! (not open?))
-                                            (if open?
-                                              (schedule-close)
-                                              (keep-open)))
+                                 :onClick (fn [] (set-open! (not open?)))
                                  :tabIndex 0
-                                 :onFocus (fn []
-                                            (set-open! true)
-                                            (keep-open))
-                                 :onBlur (fn []
-                                           (schedule-close))})
+                                 :onMouseEnter (fn [] (println "onMouseEnter1") (set-open! true))
+                                 :onMouseLeave (fn [] (println "onMouseLeave1") (schedule-close))})
        (when open?
          ($ :div.absolute.bottom-full.mb-2.w-64.bg-gray-800.text-white.text-xs.rounded.py-2.px-3.shadow-lg.z-50
-            {:onMouseEnter (fn []
-                             (setIs-hovering! true)
-                             (keep-open))
-             :onMouseLeave (fn []
-                             (setIs-hovering! false)
-                             (schedule-close))}
+            {:onMouseEnter (fn [] (println "onMouseEnter2") (set-is-hovering! true))
+             :onMouseLeave (fn [] (println "onMouseLeave2") (set-is-hovering! false))}
             "A JSONPath expression to extract a value from the input/output JSON object."
             ($ :br)
             ($ :a.text-blue-300.hover:underline.cursor-pointer
