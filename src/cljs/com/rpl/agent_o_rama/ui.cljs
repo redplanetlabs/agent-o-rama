8c2deb789ef56772af6b7f51d3ce5032e9039693
Tommy Morriss <thmorriss@gmail.com>
2025-09-09 17:42:26 -0700

    nice

--- src/cljs/com/rpl/agent_o_rama/ui.cljs
+++ src/cljs/com/rpl/agent_o_rama/ui.cljs
@@ -313,7 +313,10 @@
         view-stack (get-in match [:data :views] [])
         app-component (reduce (fn [child-component parent-component]
                                 (fn [props]
-                                  ($ parent-component (merge props {:child-component child-component}))))
+                                  (let [props* (if (map? props)
+                                                 props
+                                                 (js->clj props :keywordize-keys true))]
+                                    ($ parent-component (assoc props* :child-component child-component)))))
                               nil
                               (reverse view-stack))]
     (if app-component
