7cfe9fd5a678a7ff628e8f8b27a7919030a46589
Nathan Marz <nathan@redplanetlabs.com>
2025-10-08 14:58:13 -1000

    don't throw when input/output match filters are used on invalid types

--- src/clj/com/rpl/agent_o_rama/impl/analytics.clj
+++ src/clj/com/rpl/agent_o_rama/impl/analytics.clj
@@ -284,6 +284,19 @@
   [info]
   (= :agent (:run-type info)))
 
+(defn log-regex-error
+  [t]
+  (tl/error ::regex-failure t "Regex match exception"))
+
+(defn- regex-match?
+  [regex json-path o]
+  (try
+    (some? (re-find regex (h/read-json-path o json-path)))
+    (catch Throwable t
+      (log-regex-error t)
+      false
+    )))
+
 (extend-protocol aor-types/RuleFilter
   FeedbackFilter
   (dependency-rule-names [this] #{(:rule-name this)})
@@ -323,7 +336,7 @@
   (dependency-rule-names [this] #{})
   (rule-filter-matches? [this info]
     (let [o (if (agent-run-type? info) (:invoke-args info) (:input info))]
-      (some? (re-find (:regex this) (h/read-json-path o (:json-path this))))))
+      (regex-match? (:regex this) (:json-path this) o)))
 
   OutputMatchFilter
   (dependency-rule-names [this] #{})
@@ -333,8 +346,7 @@
                        :result
                        :val)
                    (h/node->output (:result info) (:emits info)))]
-      (some? (re-find (:regex this) (h/read-json-path output (:json-path this))))
-    ))
+      (regex-match? (:regex this) (:json-path this) output)))
 
   TokenCountFilter
   (dependency-rule-names [this] #{})
