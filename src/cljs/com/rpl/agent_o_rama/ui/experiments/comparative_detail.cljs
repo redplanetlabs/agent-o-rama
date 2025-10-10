0219ffd1875d153d6add7e529a8353e2ab5f15e5
Tommy Morriss <thmorriss@gmail.com>
2025-10-10 10:23:06 -0700

    color change

--- src/cljs/com/rpl/agent_o_rama/ui/experiments/comparative_detail.cljs
+++ src/cljs/com/rpl/agent_o_rama/ui/experiments/comparative_detail.cljs
@@ -95,7 +95,7 @@
                                  is-winner? (and winning-index (= i winning-index))]
                              ($ :td {:key (str "output-" i)
                                      :className (common/cn (:td common/table-classes)
-                                                           {"bg-green-50 border-l-4 border-green-400" is-winner?})}
+                                                           {"bg-green-50" is-winner?})}
                                 (if agent-result
                                   (if (:failure? (:result agent-result))
                                     ($ :div.space-y-2
