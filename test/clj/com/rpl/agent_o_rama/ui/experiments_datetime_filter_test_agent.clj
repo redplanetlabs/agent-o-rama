2775f7282be480a61997fbb4f025e4f2fdbdc220
Tommy Morriss <thmorriss@gmail.com>
2026-01-22 09:23:08 -0800

    fix test

--- test/clj/com/rpl/agent_o_rama/ui/experiments_datetime_filter_test_agent.clj
+++ test/clj/com/rpl/agent_o_rama/ui/experiments_datetime_filter_test_agent.clj
@@ -95,7 +95,7 @@
            (aor-types/->valid-ExperimentTarget
             (aor-types/->valid-AgentTarget "DatetimeFilterTestAgent")
             {} ; metadata
-            nil)) ; input->args
+            ["$"])) ; input->args - "$" means pass the whole input as first arg
           1 ; num-repetitions
           1))] ; concurrency
 
