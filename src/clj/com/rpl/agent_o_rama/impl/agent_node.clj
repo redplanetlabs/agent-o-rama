0f19554fe0471086748f389c8423fca0b13c67e0
Tommy Morriss <thmorriss@gmail.com>
2026-04-21 12:46:10 -0500

    Revert "changed finished stream semantics"
    This reverts commit cac6ce84aa651d0fbf4cd79aaf0a3796ba064e2a.

--- src/clj/com/rpl/agent_o_rama/impl/agent_node.clj
+++ src/clj/com/rpl/agent_o_rama/impl/agent_node.clj
@@ -128,20 +128,19 @@
          )))
      StreamingRecorderInternal
      (waitFinish [this]
-       ;; Always emit FINISHED-INVOKE so client streams get a terminal callback even when
-       ;; the node never called streamChunk (index-vol stays 0).
-       (vswap! outstanding-queue-vol
-               conj
-               (foreign-append-async!
-                streaming-depot
-                (aor-types/->valid-NodeStreamingResult
-                 agent-task-id
-                 agent-id
-                 node
-                 invoke-id
-                 (identity-retry-num retry-num)
-                 (identity-streaming-index @index-vol)
-                 iclient/FINISHED-INVOKE)))
+       (when (> @index-vol 0)
+         (vswap! outstanding-queue-vol
+                 conj
+                 (foreign-append-async!
+                  streaming-depot
+                  (aor-types/->valid-NodeStreamingResult
+                   agent-task-id
+                   agent-id
+                   node
+                   invoke-id
+                   (identity-retry-num retry-num)
+                   (identity-streaming-index @index-vol)
+                   iclient/FINISHED-INVOKE))))
        (doseq [cf @outstanding-queue-vol]
          (verify-successful-cf! cf)))
     )))
