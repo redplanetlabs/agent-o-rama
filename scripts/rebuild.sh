b9b73eb86edc04a67b08b13a689bee289e9ec8d7
Andriy Tyurnikov <Andriy.Tyurnikov@gmail.com>
2026-08-05 09:33:35 +0300

    Fix rebuild.sh (#335)

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,18 +1,28 @@
-set -x 
+#!/usr/bin/env bash
+set -ex
+
+# Rama source checkout, override if it is not a sibling of this repo
+RAMA_SRC="${RAMA_SRC:-$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)/rama}"
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
 # destroy local cluster
-pushd ../rama/
+pushd "$RAMA_SRC"
 ./scripts/local-cluster/shutdown-local-cluster.sh
 rm -rf ./local-cluster
 ./scripts/local-cluster/setup-local-cluster.sh
 popd
 # compile new aor
-./scripts/build.sh
+pushd "$REPO_ROOT"
+./scripts/build-ui.sh
+lein install
+popd
 # uberjar in examples directory
-pushd ./examples/clj
+pushd "$REPO_ROOT/examples/clj"
 lein uberjar
 popd
 # ./rama --deploy
-pushd ../rama/local-cluster/client
-export JAR=/Users/tommy/programming/agent-o-rama/examples/clj/target/agent-o-rama-examples-1.0.0-SNAPSHOT-standalone.jar
-./rama deploy --action launch --jar $JAR --module com.rpl.agent.research-agent/ResearchAgentModule --workers 1 --threads 1 --tasks 1
-./rama deploy --action launch --jar $JAR --module com.rpl.agent.basic.basic-agent/BasicAgentModule --workers 1 --threads 1 --tasks 1
+JAR="$(ls -t "$REPO_ROOT"/examples/clj/target/agent-o-rama-examples-*-standalone.jar | head -n 1)"
+pushd "$RAMA_SRC/local-cluster/client"
+./rama deploy --action launch --jar "$JAR" --module com.rpl.agent.research-agent/ResearchAgentModule --workers 1 --threads 1 --tasks 1
+./rama deploy --action launch --jar "$JAR" --module com.rpl.agent.basic.basic-agent/BasicAgentModule --workers 1 --threads 1 --tasks 1
+popd
